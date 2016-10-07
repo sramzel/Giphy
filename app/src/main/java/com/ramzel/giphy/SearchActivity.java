2239e410f467ca4cc6d14afbb96e0c6254c97ab0
Steven Ramzel <sramzel@gmail.com>
2016-10-07 02:23:53 -0700

    Optimize imports

--- app/src/main/java/com/ramzel/giphy/SearchActivity.java
+++ app/src/main/java/com/ramzel/giphy/SearchActivity.java
@@ -1,26 +1,18 @@
 package com.ramzel.giphy;
 
-import android.content.Context;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.*;
 import android.support.v7.widget.SearchView;
-import android.util.AttributeSet;
+import android.support.v7.widget.Toolbar;
 import android.view.Display;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.ramzel.giphy.models.Datum;
-
-import javax.inject.Singleton;
-
-import dagger.Provides;
 
 public class SearchActivity extends AppCompatActivity implements SearchFragment.Listener {
 
--- app/src/main/java/com/ramzel/giphy/SearchFragment.java
+++ app/src/main/java/com/ramzel/giphy/SearchFragment.java
@@ -1,13 +1,10 @@
 package com.ramzel.giphy;
 
 import android.content.Context;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.SearchView;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
